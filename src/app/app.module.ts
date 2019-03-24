448fd876618e7f4e8fc0c1352dff10d24e15c220
Mikołaj Tworek <tworekm92@gmail.com>
2019-03-24 18:37:34 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CardsComponent } from './cards/cards.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    CardsComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
